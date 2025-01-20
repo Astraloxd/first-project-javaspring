3d762c4311391bcfeb38684c1b0e6bf16440106f
Kamil <perzynakamil5@gmail.com>
2025-01-20 20:23:47 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_project_javaspring/FirstProjectJavaspringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_javaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJavaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/first_project_javaspring/FirstProjectJavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_javaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
